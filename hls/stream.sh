b7d51b7b970217427d1015396f55e37bad89ab44
r1k <3041753+r1k@users.noreply.github.com>
2021-03-16 22:38:45 +0000

    WIP

--- hls/stream.sh
+++ hls/stream.sh
@@ -5,10 +5,9 @@
 #        v4l2src device=/dev/video0 \
 #        ! videoconvert ! videoscale ! video/x-raw,width=320,height=240 \
 #        ! clockoverlay shaded-background=true font-desc="Sans 38" \
-#        ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080
+#        ! theoraenc ! oggmux ! tcpserversink host=0.0.0.0 port=8080
 
-gst-launch-1.0 rtspsrc location="rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov" ! \
-        rtph264depay ! mpegtsmux ! \
-        hlssink playlist-root=http://192.168.221.66:8080 location=/root/hls/segment_%05d.ts target-duration=5 max-files=5
+gst-launch-1.0 filesrc location="BISS-CA-32Keysin3rdSrvc.ts" ! \
+        hlssink playlist-root=http://rkmbpro16.local:8080 location=/root/hls/segment_%05d.ts target-duration=5 max-files=5
         
 echo "Stopped"
